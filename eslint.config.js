67be26df4d3c8f1bbebf4eea309e04ac6c204e32
Abreham <abrehamyetwale@gmail.com>
2025-12-16 19:09:11 +0300

    update existing .eslintrc.cjs to turn off the rule 2

---
+++ eslint.config.js
@@ -0,0 +1,95 @@
+//eslint.config.js
+const js = require('@eslint/js');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    // TypeScript files
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        Express: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      'no-unused-vars': 'off', // Turn off core rule
+      '@typescript-eslint/no-unused-vars': [
+        'warn', // Change to warn instead of error
+        { 
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_' 
+        }
+      ],
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/ban-ts-comment': 'warn'
+    }
+  },
+  {
+    // Test files - disable most rules
+    files: ['**/__tests__/**', '**/*.test.ts', '**/*.spec.ts'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'no-console': 'off',
+      'eqeqeq': 'off',
+      'curly': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off'
+    }
+  },
+  {
+    // CommonJS files (like config files)
+    files: ['**/*.js', '**/*.cjs'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+        module: 'readonly',
+        require: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-undef': 'off',
+      'no-console': 'off'
+    }
+  },
+  {
+    // Configuration files
+    files: ['*.config.js', '*.config.cjs'],
+    rules: {
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-undef': 'off',
+      'no-console': 'off'
+    }
+  },
+  {
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'coverage/**',
+      '*.config.js',
+      '*.config.cjs'
+    ]
+  }
+];
